553b238154551ba4536bba0ca26cbeaa4d12d3aa
Nguyễn Thanh Tâm <tabenguyen@gmail.com>
2015-11-17 00:47:35 +0000

    #36 Config db for test enironment

--- config/database.php
+++ config/database.php
@@ -78,13 +78,13 @@ return [
         ],
         'testing' => array(
             'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => env('DB_HOST', 'ds041603.mongolab.com'),
+            'port'     => env('DB_PORT', 41603),
+            'database' => env('DB_DATABASE', 'reecms'),
+            'username' => env('DB_USERNAME', 'reecms'),
+            'password' => env('DB_PASSWORD', 'reecms'),
             'options' => array(
-                'db' => env('DB_OPTIONS_DB', 'admin')
+                'db' => env('DB_OPTIONS_DB', 'reecms')
             )
         ),
         'mongodb' => array(
